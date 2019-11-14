40bdafdaaf8b65af784fe220a1fabce578a1f2cf
Adam Torkkeli <adam.torkkeli@getaccept.com>
2019-11-14 15:20:36 +0100

    added cors middleware

--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/aws/aws-sdk-go v1.25.34
+	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/google/uuid v1.1.1
 )
--- backend/server/server.go
+++ backend/server/server.go
@@ -7,13 +7,20 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joakimsoren/screamdeal/backend/awsutils"
 	// "reflect"
-	"io"
-	"io/ioutil"
-	"bytes"
+	// "io"
+	// "io/ioutil"
+	// "bytes"
+	"github.com/gin-contrib/cors"
+	// "unsafe"
 )
 
 func StartServer() {
 	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:8081"}
+	// config.AllowOrigins == []string{"http://google.com", "http://facebook.com"}
+
+	router.Use(cors.New(config))
 	setupThemes(router)
 	setupPutPdf(router)
 	setupAddThemeToPds(router)
@@ -33,15 +40,22 @@ func setupThemes(router *gin.Engine) {
 
 func setupPutPdf(router *gin.Engine) {
 	router.POST("/put-pdf", func(c *gin.Context) {
-		fileHeader, _ := c.FormFile("filepdf")
-		file, _ := fileHeader.Open() //get the file
-		fileTemp, _ := ioutil.TempFile(".", "temp") //create a temporary file
-		buffer := bytes.NewBuffer(nil) //create empty buffer
-		io.Copy(buffer, file) //write file to buffer
-		fileTemp.Write(buffer.Bytes())//write buffer to temp file.
-		// fmt.Println(reflect.TypeOf(buffer.Bytes()))
-		// fmt.Println(fileTemp.)
-		// fmt.Println(reflect.TypeOf(fileTemp))
+		// fileHeader, _ := c.FormFile("filepdf")
+		// file, _ := fileHeader.Open() //get the file
+		// // fileTemp, _ := ioutil.TempFile(".", "temp") //create a temporary file
+		// buffer := bytes.NewBuffer(nil) //create empty buffer
+		// io.Copy(buffer, file) //write file to buffer
+		// message := []byte("Hello, Gophers!")
+		// error := ioutil.WriteFile("testdata/hello.pdf", message, 0644)
+		// fmt.Println(error)
+		// // fileTemp.Write(buffer.Bytes())//write buffer to temp file.
+		// // stat, _ := fileTemp.Stat()
+		// // fmt.Println(stat.Name(), stat.Size())
+		// // fmt.Println(reflect.TypeOf(buffer.Bytes()))
+		// // fmt.Println(unsafe.Sizeof(buffer.Bytes()))
+		// // fmt.Println(fileTemp.Stat().Size())
+		// // fmt.Println(reflect.TypeOf(fileTemp))
+		// // awsutils.UploadFile(fileTemp) 
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.JSON(200, gin.H{
 			"url": "url to pdf in S3",
