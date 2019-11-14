0a710abc162317d3496134874673f6975fcafdc9
Isac Ljung <isac.ljung@getaccept.com>
2019-11-14 16:09:30 +0100

    LAst fixes

--- frontend/src/views/Download.vue
+++ frontend/src/views/Download.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="download">
     <h1>The deal has been scarified, you may take it...</h1>
-    <button class="download-monster">Fetch the monster</button>
+    <a :href="url" target="_blank" class="download-monster button"
+      >Fetch the monster</a
+    >
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
+import { State, Action } from "vuex-class";
+import { namespace as themeNamespace } from "@/store/themes/theme.store";
 
 @Component
 export default class Download extends Vue {
-  @Prop() url: string;
+  @State("themedPdf", { namespace: themeNamespace }) url: string;
+
+  mounted() {
+    if (!this.url) {
+      this.$router.go(-1);
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -22,3 +32,5 @@ export default class Download extends Vue {
   }
 }
 </style>
+
+https://scary-bucket.s3.eu-west-1.amazonaws.com/themed-documents/awesome-o.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAZD335QTAZ6FZMZU3%2F20191114%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20191114T145835Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=2cf2830278f3f7142b85e9cb3c7f66b3858ddda2fd9812ad2978748bee14e948
